604eb4b27a865b7882f41bb8936bab72816d3d1d
vanik-jain <jainvanik100@gmail.com>
2020-01-19 14:33:34 +0530

    initial commit

---
+++ app/src/main/java/com/example/baniya/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.baniya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.baniya.landing.LandingActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ActionBar actionBar = getActionBar();
+//        if (actionBar != null)
+//        actionBar.hide();
+
+        new Handler().postDelayed
+                (new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                            Intent intent = new Intent(MainActivity.this, LandingActivity.class);
+                            startActivity(intent);
+
+                        finish();
+                    }
+                }, 700);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/baniya/landing/LandingActivity.java
@@ -0,0 +1,44 @@
+package com.example.baniya.landing;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.baniya.R;
+
+public class LandingActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        else {
+
+            super.onBackPressed();
+        }
+    }
+}
